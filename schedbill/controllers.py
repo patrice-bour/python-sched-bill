f3bf58c6b3e7d20844cb31a89fff1ca3b1862255
b0rice <patrice.bour.71@gmail.com>
2022-06-04 23:53:31 +0200

    Fixed scheduling mechanism for emails. Added functions / methods signatures

--- schedbill/controllers.py
+++ schedbill/controllers.py
@@ -3,19 +3,18 @@ from mongoengine import ValidationError, DoesNotExist
 from bson import ObjectId
 from bson.errors import InvalidId
 from models import User, EMail, Invoice
-from datetime import datetime
-import pytz
+from timing import TimeCalc
 import logging
 
 
 logger = logging.getLogger()
 
 
-class UserController():
+class UserController:
     """"""
 
     @classmethod
-    def find(cls, oid):
+    def find(cls, oid: str) -> User:
         """Find a user and return it
 
         :param oid: the ObjectId of the document
@@ -29,7 +28,7 @@ class UserController():
         return user
 
     @classmethod
-    def find_by_email(cls, email_address):
+    def find_by_email(cls, email_address: str):
         """Find a user and return it
 
         :param email_address: the email address of the document
@@ -41,27 +40,27 @@ class UserController():
         return user
 
     @classmethod
-    def create_user(cls):
+    def create_user(cls, raw_user: dict) -> User:
         """Create a new user and return it
 
+        :param raw_user: the properties of the document
         :return: the new User
         """
 
-        raw_user = request.get_json()
         user = User(**raw_user)
         user.save()
 
         return user
 
     @classmethod
-    def update_user(cls, oid):
+    def update_user(cls, oid: str, updated_user: dict) -> User:
         """Update a user and return it
 
         :param oid: the ObjectId of the document
+        :param updated_user: the properties to update in the document
         :return: the updated User
         """
 
-        updated_user = request.get_json()
         if not ObjectId.is_valid(oid):
             raise InvalidId(f"Invalid ObjectId '{oid}'")
         user = User.objects(id=oid).modify(**updated_user)
@@ -71,7 +70,7 @@ class UserController():
         return user
 
     @classmethod
-    def delete_user(cls, oid):
+    def delete_user(cls, oid: str) -> None:
         """Delete a user
 
         :param oid: the ObjectId of the document
@@ -84,11 +83,11 @@ class UserController():
         user.delete()
 
 
-class EMailController():
+class EMailController:
     """"""
 
     @classmethod
-    def find(cls, oid):
+    def find(cls, oid: str) -> EMail:
         """Find a email and return it
 
         :param oid: the ObjectId of the document
@@ -102,40 +101,46 @@ class EMailController():
         return email
 
     @classmethod
-    def create_email(cls):
+    def create_email(cls, raw_email: dict) -> EMail:
         """Create a new email and return it
 
+        :param raw_email: the properties of the document
         :return: the new EMail
         """
 
-        raw_email = request.get_json()
         # Reckon sending time
-
+        if raw_email.get('sendAt', 0) > 0:
+            raw_email['sendAt'] = TimeCalc.convert_to_store(raw_email['sendAt'])
         email = EMail(**raw_email)
         email.save()
+        cls.schedule_email(email)
 
         return email
 
     @classmethod
-    def update_email(cls, oid):
-        """Update a email and return it
+    def update_email(cls, oid: str, updated_email: dict) -> EMail:
+        """Update an email and return it
 
+        :param updated_email: the properties to update in the document
         :param oid: the ObjectId of the document
         :return: the updated EMail
         """
 
-        updated_email = request.get_json()
         if not ObjectId.is_valid(oid):
             raise InvalidId(f"Invalid ObjectId '{oid}'")
+        # Reckon sending time
+        if updated_email.get('sendAt', 0) > 0:
+            updated_email['sendAt'] = TimeCalc.convert_to_store(updated_email['sendAt'])
         email = EMail.objects(id=oid).modify(**updated_email)
         if not email:
             raise DoesNotExist(f"Can not find email with id '{oid}'")
+        cls.schedule_email(email)
 
         return email
 
     @classmethod
-    def delete_email(cls, oid):
-        """Delete a email
+    def delete_email(cls, oid: str) -> None:
+        """Delete an email
 
         :param oid: the ObjectId of the document
         """
@@ -143,33 +148,46 @@ class EMailController():
         if not ObjectId.is_valid(oid):
             raise InvalidId(f"Invalid ObjectId '{oid}'")
         email = EMail.objects.get(id=oid)
+        cls.schedule_email(email)
+
         email.delete()
 
     @classmethod
-    def schedule_email(cls, email):
+    def schedule_email(cls, email: EMail) -> int:
         """"""
-
         if email.sendAt > 0:
-            g.scheduler.add_job(cls.send_email(), '', args=email.id, id=email.id, replace_existing=True)
+            # replace_existing is set to true. This would then also update an existing job's schedule.
+            g.scheduler.add_job(EMailController.send_email, 'date', args=[str(email.id)], id=str(email.id),
+                                replace_existing=True)
+            return 1
         else:
-            try:
-                g.scheduler.remove_job(email.id)
-            except Exception as exc:
-                raise Exception(exc)
-
+            # any existing job's schedule has to be deleted.
+            ret = cls.unschedule_email(email)
+            return ret if ret == 0 else -1
 
     @classmethod
-    def send_email(cls, oid):
+    def unschedule_email(cls, email: EMail) -> int:
+        """"""
+
+        if g.scheduler.get_job(str(email.id)) is not None:
+            g.scheduler.remove_job(str(email.id))
+            return 1
+        else:
+            return 0
+
+    @classmethod
+    def send_email(cls, oid: str) -> None:
         """"""
 
         email = cls.find(oid)
         logger.info(f"*** Sending email : {email.to_json()}")
 
-class InvoiceController():
+
+class InvoiceController:
     """"""
 
     @classmethod
-    def find(cls, oid):
+    def find(cls, oid: str) -> Invoice:
         """Find a invoice and return it
 
         :param oid: the ObjectId of the document
@@ -183,27 +201,27 @@ class InvoiceController():
         return invoice
 
     @classmethod
-    def create_invoice(cls):
+    def create_invoice(cls, raw_invoice: dict) -> Invoice:
         """Create a new invoice and return it
 
+        :param raw_invoice: the properties of the document
         :return: the new Invoice
         """
 
-        raw_invoice = request.get_json()
         invoice = Invoice(**raw_invoice)
         invoice.save()
 
         return invoice
 
     @classmethod
-    def update_invoice(cls, oid):
+    def update_invoice(cls, oid: str, updated_invoice: dict) -> Invoice:
         """Update a invoice and return it
 
         :param oid: the ObjectId of the document
+        :param updated_invoice: the properties to update in the document
         :return: the updated Invoice
         """
 
-        updated_invoice = request.get_json()
         if not ObjectId.is_valid(oid):
             raise InvalidId(f"Invalid ObjectId '{oid}'")
         invoice = Invoice.objects(id=oid).modify(**updated_invoice)
@@ -213,7 +231,7 @@ class InvoiceController():
         return invoice
 
     @classmethod
-    def delete_invoice(cls, oid):
+    def delete_invoice(cls, oid: str) -> None:
         """Delete a invoice
 
         :param oid: the ObjectId of the document
